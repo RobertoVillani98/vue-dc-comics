305b12d020ce08f5ff59da6b558b81ab6f11b9ad
RobertoVillani <robertovillani58@gmail.com>
2021-12-13 18:26:34 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="Logo dc"/>
+        <nav>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/partials/variables.scss';
+header {
+  margin-top: 20px;
+  text-align: center;
+  display: flex;
+  margin-bottom: 20px;
+}
+.container {
+  width: 80%;
+  margin: 0 auto;
+}
+.logo {
+  display: flex;
+  img {
+    height: 70px;
+  }
+}
+nav {
+  margin-top: 10px;
+  ul {
+    display: flex;
+    list-style: none;
+    li a {
+      text-transform: uppercase;
+      font-weight: 500;
+      font-size: 18px;
+      display: inline-block;
+      padding: 15px 20px;
+      text-decoration: none;
+      color: #000;
+      &.active,
+      &:hover {
+        background-color: $mainColor;
+      }
+    }
+  }
+}
+
+</style>
